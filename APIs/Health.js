ffaceada94ea1121790d9876ab0708498393e505
Riya Soni <156263230+rsoni89@users.noreply.github.com>
2024-03-17 21:01:55 -0400

    Create Health.js

---
+++ APIs/Health.js
@@ -0,0 +1,14 @@
+
+console.log("Download the app 'MyCharts' that would have all the reports, documents.")
+console.log();
+console.log();
+console.log("Immunization Requirement :- https://med.uc.edu/landing-pages/university-health/vaccination/college-of-medicine-md-immunization-requirements"); //Immunizations 
+console.log(); //for blank line
+console.log("Immunization Requirement for International Students :- https://med.uc.edu/landing-pages/university-health/vaccination/international-student-immunization-requirements"); //Immunization for Internation Students
+console.log();
+console.log("Mental Health :- https://med.uc.edu/landing-pages/university-health/emergencysituations/mentalhealth"); //Mental Health
+console.log(); //for blank line
+console.log("Emergencies: Please call 911"); //Emergency line 
+console.log();
+console.log("There are off campus  emergency numbers available as well."); //Off campus emergency
+console.log();
