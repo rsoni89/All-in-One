9e7fef8e0ad0c2def8a3825010b0d0f2f19842a5
Aleny Moreno <156278080+morenoadc@users.noreply.github.com>
2024-03-29 12:51:06 -0400

    All

--- APIs/Education.js
+++ APIs/Education.js
@@ -1,40 +1,42 @@
 console.log("What are coops?");
-console.log();
 console.log("How to prepare for coops?");
-//
 console.log("What is the minimum requirement for qualifing for coops?"); // rephrase
-//
 console.log("Different ways to practice for technical interviews: ")// Heading 
 // Bullet points for different ways 
-// 1) Mock interviews 
-// 2) Practice problems from websites like leetcode, hackerrank etc.
-// 3) Practing coding with different projects on GitHub
-// 4) Networking and reaching out to people in your field
-// 5) Researching companies you're interested in and understanding
-// 6) Taking online courses or certifications related to your field 
-
+{
+   const list1 = [
+ "1) Mock interviews", 
+ "2) Practice problems from websites like leetcode, hackerrank, etc...",
+ "3) Practicing coding with different projects on GitHub",
+ "4) Networking and reaching out to people in your field",
+"5) Researching companies you're interested in and understanding",
+"6) Taking online courses or certifications related to your field",
+   ] 
+};
 console.log("How to land coops?"); // necessary steps to take for it 
 // 1) Focus on the  skills that are most important for your desired role 
 // 2) Make sure you have a strong resume - tail
 // 3) Be active on linkedin, Handshake, Pal 
 // 4) Go to resume builiding sessions
 console.log();
-console.log("ACCEND Program - Masters and Bacehlors") // Different majors and  programs available in ACCEND program
+console.log("ACCEND Program - Masters and Bachelors") // Different majors and  programs available in ACCEND program
 console.log("Grad Schools in United States"); // Do we do PHD or go to grad School 
 console.log("Internships vs Co-Ops") // What's the difference between internship and coop
 console.log("Internships are short-term, typically lasting a few weeks to a few months, while co-ops (Cooperative Education) are long-term, lasting multiple semesters with alternating work and study terms. Internships may or may not be for academic credit, while co-ops are often integrated with academic curriculum and for academic credit.")
-console.log("Internships provide practical work experience related to the student's field, while co-ops offer more extensive and structured work experience. Interns are often unpaid or receive minimal compensation, whereas co-op participants are considered employees and may receive compensation and benefits. Internships primarily focus on gaining work experience and exploring career paths, while co-ops aim to integrate classroom learning with practical work experience and provide deeper insights into the field. ");
-console.log(); //blank line
+console.log("Internships provide practical work experience related to the student's field, while co-ops offer more extensive and structured work experience. Interns are often unpaid or receive minimal compensation, whereas co-op participants are considered employees and may receive compensation and benefits. Internships primarily focus on gaining work experience and exploring career paths, while co-ops aim to integrate classroom learning with practical work experience and provide deeper insights into the field. ")
 console.log("Minors for the  future") // what can you add on top of your major, like a minor in computer science?
-// Computer Science 
-// Mathematics 
-// Robotics and Automation 
-// Emedded  Systems 
+{
+ const list3 =  [
+ "Computer Science", 
+ "Mathematics",
+ "Robotics and Automation",
+ "Embedded Systems",
+ ]
+};
 // 
 console.log("Majors and curriculum for different majors") //Electrical and Computer Engineering provide links 
-console.log("Electrical Engineering: //link........."); //link for the curriculum
-console.log("Computer Engineering: //link........."); //link for curriculum
-console.log("Classes planning and scheduling!")
+console.log("Electrical Engineering: https://ceas.uc.edu/academics/departments/electrical-computer-engineering/degrees-programs/electrical-engineering-bachelor-of-science.html "); //link for the curriculum
+console.log("Computer Engineering: https://ceas.uc.edu/academics/departments/electrical-computer-engineering/degrees-programs/computer-engineering-bachelor-of-science.html"); //link for curriculum
 
 
 
