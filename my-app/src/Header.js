7781a39f246b04d7039a03f6cea0c38ca2717d45
Spencer Abma <153416733+Abmaspencer@users.noreply.github.com>
2024-04-10 20:33:59 +0000

    pepe

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,13 +1,22 @@
 // Header.js
-import React from 'react';
-import './Header.css'; // Assuming you will create a CSS file for styling
 
-function Header() {
-  return (
-    <div className="header">
-      All In One
-    </div>
-  );
-}
+import React from 'react';
+
+const Header = () => {
+    return (
+      <header className="App-css" style={{ textAlign: 'center', backgroundColor: 'black', color: 'white' }}>
+        <div> {/* Wrap title and nav */}
+          <h1>UC All in One</h1>
+          <nav>
+            <a href="/" className="nav-link">Home</a>
+            <a href="/about" className="nav-link">About</a>
+            <a href="/contact" className="nav-link">Contact</a>
+          </nav>
+        </div>
+      </header>
+    );
+  };
+  
+  
 
 export default Header;
